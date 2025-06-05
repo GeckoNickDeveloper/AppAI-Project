ec4e92d417f3686271235364b8f53f1170e71506
GeckoNickDeveloper <nicholas.nick.kania.development@gmail.com>
2025-06-06 00:26:10 +0200

    Software Update
    > Added base classes

---
+++ ae.py
@@ -0,0 +1,15 @@
+# Imports
+import torch.nn as nn
+
+# Network definition
+## Encoder
+class Encoder(nn.Module):
+    pass
+
+## Decoder
+class Decoder(nn.Module):
+    pass
+
+## AutoEncoder
+class AutoEncoder(nn.Module):
+    pass
