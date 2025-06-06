b5e68f25d99b92f8b310ac9dc9ba3f5ce0a6c59c
GeckoNickDeveloper <nicholas.nick.kania.development@gmail.com>
2025-06-06 20:05:27 +0200

    Software Update
    > Added the code base of the AutoEncoder

--- ae.py
+++ ae.py
@@ -4,12 +4,45 @@ import torch.nn as nn
 # Network definition
 ## Encoder
 class Encoder(nn.Module):
-    pass
+    """ TODO: add doc """
+    def __init__(self, arg):
+        super(Encoder, self).__init__()
+        self.arg = arg
+
+    """ TODO: add doc """
+    def forward(self, inputs):
+        pass
+
+
 
 ## Decoder
 class Decoder(nn.Module):
-    pass
+    """ TODO: add doc """
+    def __init__(self, arg):
+        super(Decoder, self).__init__()
+        self.arg = arg
+
+    """ TODO: add doc """
+    def forward(self, inputs):
+        pass
+
+
 
 ## AutoEncoder
 class AutoEncoder(nn.Module):
-    pass
+    """ TODO: add doc """
+    def __init__(self):
+        super(AutoEncoder, self).__init__()
+        
+        # Components
+        self.encoder = Encoder()
+        self.decoder = Decoder()
+
+    """ TODO: add doc """
+    def forward(self, inputs):
+        # Encode
+        embedding = self.encoder(inputs)
+        # Decode
+        out = self.decoder(embedding)
+
+        return out
